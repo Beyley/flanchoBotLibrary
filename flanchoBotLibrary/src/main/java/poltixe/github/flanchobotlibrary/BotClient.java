449e391790c40a6d49a9790796dddc063e22dbe0
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-02-19 07:55:48 -0800

    Fix logins and packet reading

--- flanchoBotLibrary/src/main/java/poltixe/github/flanchobotlibrary/BotClient.java
+++ flanchoBotLibrary/src/main/java/poltixe/github/flanchobotlibrary/BotClient.java
@@ -535,7 +535,7 @@ public abstract class BotClient {
 
             this.console.printMessage("Authenticating...");
 
-            this.client.send(String.format("%s\r\n%s\r\n1400|%d|%s\r\n", this.username, this.password,
+            this.client.send(String.format("%s\r\n%s\r\n999999|%d|%s\r\n", this.username, this.password,
                     Time.getCurrentTimezone(), this.showLocationData ? 1 : 0));
 
             this.lastPingTime = Time.now();
